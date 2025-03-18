3b68144f04a908d372d565614a214a64b66f5564
Pooyan Khanjankhani <pooyankhan@gmail.com>
2025-03-18 16:51:20 +0330

    experimentally add solidjs

---
+++ web/bundler/index.ts
@@ -0,0 +1,16 @@
+import { build } from "esbuild";
+import { solidPlugin } from "esbuild-plugin-solid";
+
+const env = process.env.NODE_ENV ?? "dev";
+
+build({
+  entryPoints: ["src/index.tsx"],
+  bundle: true,
+  sourcemap: true,
+  outfile: "dist/bundle.js",
+  plugins: [solidPlugin()],
+  define: {
+    "process.env.NODE_ENV": JSON.stringify(env),
+  },
+  conditions: [env],
+}).catch(() => process.exit(1));
---
+++ web/src/index.tsx
@@ -0,0 +1,7 @@
+import { render } from "solid-js/web";
+
+function App() {
+  return <div>Hello App</div>;
+}
+
+render(() => <App />, document.getElementById("app")!);
