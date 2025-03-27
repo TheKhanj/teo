189f7a27b9a3f31c52013300a64eed8461ac2d2b
Pooyan Khanjankhani <pooyankhan@gmail.com>
2025-03-27 23:59:17 +0330

    handle graceful shutdown

--- api/teo.go
+++ api/teo.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -19,10 +25,48 @@ func runHttpServer(config *Config) {
 	fullAddress := fmt.Sprintf("%s:%d", *config.Api.Address, *config.Api.Port)
 	log.Printf("listening on address %s", fullAddress)
 
-	err := http.ListenAndServe(fullAddress, router)
-	if err != nil {
-		log.Fatal(err)
+	server := &http.Server{
+		Addr:    fullAddress,
+		Handler: router,
 	}
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		handleGracefulShutdown(server)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+
+	wg.Wait()
+}
+
+func handleGracefulShutdown(server *http.Server) {
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	signal.Notify(stop, syscall.SIGTERM)
+	signal.Notify(stop, syscall.SIGINT)
+
+	<-stop
+
+	log.Println("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("server forced to shutdown: %v\n", err)
+	}
+
+	log.Println("server stopped")
 }
 
 func main() {
