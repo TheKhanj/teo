ca32ee233e30de13721235107e96f54671a86963
Pooyan Khanjankhani <pooyankhan@gmail.com>
2025-03-27 16:10:58 +0330

    remove junk from frotend api

--- web/src/providers/api.tsx
+++ web/src/providers/api.tsx
@@ -1,44 +1,10 @@
 import { createContext, FlowProps, useContext } from "solid-js";
 
 import { Oath, To, to } from "../util";
-import { sessionGet, sessionSet } from "../session";
 
 export class Api {
-  private readonly headers = new Headers();
-
-  public constructor() {
-    const auth = sessionGet("authorization-header");
-    if (auth) this.headers.set("Authorization", auth);
-  }
-
-  public async authLogin(
-    username: string,
-    password: string,
-  ): Oath<Error | null> {
-    const auth = `Basic ${btoa(`${username}:${password}`)}`;
-    const [res, err] = await to(
-      fetch("/api/auth/login", {
-        headers: new Headers({ Authorization: auth }),
-      }),
-    );
-
-    if (err !== null) return err;
-    if (res.ok === false) {
-      const [text, err] = await to(res.text());
-      if (err != null) return err;
-      return new Error(text);
-    }
-
-    this.headers.set("Authorization", auth);
-    sessionSet("authorization-header", auth);
-
-    return null;
-  }
-
   public async cameras(): Oath<To<string[], Error>> {
-    const [res, err] = await to(
-      fetch("/api/cameras/", { headers: this.headers }),
-    );
+    const [res, err] = await to(fetch("/api/cameras/"));
     if (err !== null) return [null, err];
 
     if (!res.ok) {
--- web/src/session.ts
+++
@@ -1,9 +0,0 @@
-export type SessionKey = "authorization-header";
-
-export function sessionGet(key: SessionKey) {
-  return sessionStorage.getItem(key);
-}
-
-export function sessionSet(key: SessionKey, value: string) {
-  sessionStorage.setItem(key, value);
-}
